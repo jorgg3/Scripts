3cd2026f7b2bdad8d5962141dbeaaa374318cd9f
jorgg3 <jo_rgge@ciencias.unam.mx>
2026-01-12 18:19:02 -0600

    Update detection_only.py
    Se actualizó el script de ejecución (detection_only.py) para adaptarlo al entorno del servidor con configuraciones de hardware y software que pueden variar. Se agregó la lógica try-except para evitar errores de congelamiento.

--- detection_only.py
+++ detection_only.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from argparse import ArgumentParser
 from PytorchWildlife.models import detection as pw_detection
+import torch
 from utils import process_detection_only, process_folder_detection_only
 
 # Detectores probados basados en los disponibles en https://microsoft.github.io/CameraTraps/model_zoo/megadetector/
@@ -12,23 +13,35 @@ DETECTORS = {
     "RtDetr_Compact": "MDV6-rtdetr-c"
 }
 DETECTORS_OPTS = list(DETECTORS.keys())
-
+def get_device():
+    if torch.cuda.is_available():
+        print(f"Dispositivo GPU: {torch.cuda.get_device_name(0)}")
+        return "cuda"
+    else:
+        print("Dispositivo CPU .")
+        return "cpu"
 if __name__ == "__main__":
     parser = ArgumentParser(
         prog="detection_tool",
         description="Herramienta exclusiva para detección y recorte de animales, permitiendo elegir el modelo."
     )
     parser.add_argument("path", type=Path, help="Ruta de la imagen o carpeta a procesar")
-    parser.add_argument("--detector", default=DETECTORS_OPTS[0], choices=DETECTORS_OPTS, 
-                        help="Modelo de detección que se desea usar")
+    parser.add_argument("--detector", default=DETECTORS_OPTS[0], choices=DETECTORS_OPTS, help="Modelo de detección que se desea usar")
     parser.add_argument("--margin", type=int, default=5, help="Tamaño del margen alrededor de la detección")
-
     args = parser.parse_args()
-
+    device = get_device()
     # Cargaremos el modelo y se imprimirá la elección
-    print(f"\nCargando detector: {args.detector}")
-    detector = pw_detection.MegaDetectorV6(version=DETECTORS[args.detector])
-
+    model_version = DETECTORS[args.detector]
+    try:
+        if "MDV6" in model_version:
+             detector = pw_detection.MegaDetectorV6(device=device, pretrained=True, version=model_version)
+        else:
+             detector = pw_detection.MegaDetectorV5(device=device, pretrained=True, version=model_version)
+        print(f"Modelo {model_version} cargado  en {device.upper()}.")
+    except Exception as e:
+        print(f" Error cargando el modelo: {e}")
+        sys.exit(1) 
+        
     # Si path es directorio se procesan todas las imagenes en el directorio
     if args.path.is_dir():
         # process_folder_detection_only ya se encarga de crear la carpeta de salida internamente
